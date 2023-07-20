cec1ee42c7ae2b03baf6bca5f3c6688a1e99189e
melodic456 <Ruben.Wheller5551@proton.me>
2023-07-21 03:02:21 +0600

    run how many times? added

--- start.sh
+++ start.sh
@@ -9,4 +9,14 @@ export SCREEN_WIDTH=1920
 export SCREEN_HEIGHT=1080
 #export PROFILE_ID=yU0Pr0f1leiD
 #export TOKEN=yU0token
-docker run -e DJANGO_IP=$DJANGO_IP -e SCREEN_WIDTH=$SCREEN_WIDTH -e SCREEN_HEIGHT=$SCREEN_HEIGHT -e PROFILE_ID=$PROFILE_ID -e TOKEN=$TOKEN --rm -ti -p 5901:5901 -p 3000:3000 --name orbita-browser gologin-test4
+
+#function run
+run() {
+    number=$1
+    shift
+    for i in `seq $number`; do
+      $@
+    done
+}
+
+run 25 docker run -e DJANGO_IP=$DJANGO_IP -e SCREEN_WIDTH=$SCREEN_WIDTH -e SCREEN_HEIGHT=$SCREEN_HEIGHT -e PROFILE_ID=$PROFILE_ID -e TOKEN=$TOKEN --rm -ti -p 5901:5901 -p 3000:3000 gologin-test4
